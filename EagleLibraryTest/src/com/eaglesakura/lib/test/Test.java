2a3b56c8bc8efdeceb31ce898b80e6e87f803377
eaglesakura <eagle.sakura@gmail.com>
2011-07-08 17:38:37 +0900

    プロジェクト追加

---
+++ EagleLibraryTest/src/com/eaglesakura/lib/test/Test.java
@@ -0,0 +1,12 @@
+package com.eaglesakura.lib.test;
+
+import android.test.AndroidTestCase;
+import android.test.suitebuilder.annotation.MediumTest;
+
+public class Test extends AndroidTestCase {
+
+    @MediumTest
+    public void test() {
+        System.out.println("Hello test!!");
+    }
+}
